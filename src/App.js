a0bf1549d6adca7d9ea51c2e5e08a0389e9766f9
obaida <obaidashurbaji99@gmail.com>
2022-09-20 14:39:37 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+
+
+const App = () => {
+	
+
+	return (
+		<div className="app">
+			
+		</div>
+	);
+};
+
+export default App;
