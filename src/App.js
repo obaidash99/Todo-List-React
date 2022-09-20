c8cc02df47347f0a6b46c888ebae04a2d3b5e120
obaida <obaidashurbaji99@gmail.com>
2022-09-20 16:33:55 +0200

    setting things up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
-
-
+// Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 const App = () => {
-	
+	const [inputText, setInputText] = useState('');
 
 	return (
 		<div className="app">
-			
+			<header>
+				<h1>Ed's Todo List</h1>
+			</header>
+			<Form setInputText={setInputText} />
+			<TodoList />
 		</div>
 	);
 };
