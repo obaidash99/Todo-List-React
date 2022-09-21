576fe4b75a0c8862b4949822358f12134741b504
obaida <obaidashurbaji99@gmail.com>
2022-09-21 10:29:36 +0200

    delete and completed btn

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Todo from './Todo.js';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
 	return (
 		<div className="todo-container">
 			<ul className="todo-list">
 				{todos.map((todo) => (
-					<Todo text={todo.text} id={todo.id} />
+					<Todo
+						text={todo.text}
+						key={todo.id}
+						todos={todos}
+						setTodos={setTodos}
+						todo={todo}
+					/>
 				))}
 			</ul>
 		</div>
